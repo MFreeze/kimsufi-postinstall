567fe141493a019dcc63e7083c29dce65da0055c
DUVILLIE Guillerme <guillerme@duvillie.eu>
2021-07-16 02:38:45 +0200

    Fix small typos

--- ovh-install.sh
+++ ovh-install.sh
@@ -9,31 +9,30 @@ GITHUB_REPO=kimsufi-postinstall
 GITHUB_BRANCH=main
 
 # Remote scripts URI root
-export URI_ROOT="https://raw.githubusercontent.com/"\
-    "${GITHUB_USER}/"\
-    "${GITHUB_REPO}/"\
-    "${GITHUB_BRANCH}/"
+export URI_ROOT="https://raw.githubusercontent.com/${GITHUB_USER}/${GITHUB_REPO}/${GITHUB_BRANCH}/"
 
 # Output log file
 LOG_FILE="/root/post-install.log"
 
 # Error codes
 ERR_OK=0
+ERR_UPDATE=1
+ERR_USERS=2
 
 
 
 # From Matthieu Michon repo
 exec_remote () {
     # 'raw.githubusercontent.com' domain may not clear HTTPS CA checks
-    bash <(wget --no-check-certificate -O - $URI_ROOT$1) > "${LOG_FILE}"
+    bash <(wget --no-check-certificate -O - $URI_ROOT$1) 2>&1 >> "${LOG_FILE}"
     return $?
 }
 
 
 main () {
-    exec_remote "scrips/update_os"
-    exec_remote "scripts/users_and_connect"
-    exit 0
+    exec_remote "scripts/update_os" || exit $ERR_UPDATE
+    exec_remote "scripts/users_and_connect" || exit $ERR_USERS
+    exit $ERR_OK
 }
 
 
